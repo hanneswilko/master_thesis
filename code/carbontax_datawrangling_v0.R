38cfafd3b3e3204ce8ae3e96fd46987c21219b03
Hannes Wilkovits <hannes.wilko@gmx.at>
2025-02-25 23:40:33 +0100

    fetching carbon tax data - next step data wrangling

--- code/carbontax_datawrangling_v0.R
+++ code/carbontax_datawrangling_v0.R
@@ -17,5 +17,15 @@ class(carbonprice_OWiD_raw)
 glimpse(carbonprice_OWiD_raw)
 View(carbonprice_OWiD_raw)
 
+##------------------------------OECD--------------------------------------------
+carbonrate_OECD_raw <- read.csv("./raw_data/OECD_carbonrates.csv")
+head(carbonrate_OECD_raw)
+class(carbonrate_OECD_raw)
+glimpse(carbonrate_OECD_raw)
+View(carbonrate_OECD_raw)
 
-
+EPS_OECD_raw <- read.csv("./raw_data/OECD_EPS.csv")
+head(EPS_OECD_raw)
+class(EPS_OECD_raw)
+glimpse(EPS_OECD_raw)
+View(EPS_OECD_raw)
