7ad0b2f92b38625b76d3e684945d864571fb50ff
Hannes Wilkovits <hannes.wilko@gmx.at>
2025-03-04 00:34:54 +0100

    EPS - visualization

--- code/desc_stats_v0.R
+++ code/desc_stats_v0.R
@@ -26,9 +26,63 @@ glimpse(EPS_data)
 wC02price_data <- read.csv("./processed_data/OWiD_C02price_data.csv")
 glimpse(wC02price_data)
 
+#------------------------Environmental Stringency-------------------------------
+##EPS
+###time frame: 2010-2020 (2020 latest observations)
+EPS_data_sub <- EPS_data %>%
+  select(REF_AREA, TIME_PERIOD, OBS_VALUE) %>%
+  filter(TIME_PERIOD %in% 2010:2020)
+
+summary(EPS_data_sub)
+
+###Density Plot for EPS per country
+ggplot(EPS_data_sub, aes(x = OBS_VALUE)) +
+  geom_density(fill = "steelblue", alpha = 0.5) +
+  labs(title = "Density Plot of EPS Values (2010-2020)", x = "EPS", y = "Density") +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.y = element_text(color = "black", size = 12),  # Y-axis text formatting
+    axis.text.x = element_text(color = "black", size = 12),  # X-axis formatting
+    axis.title = element_text(size = 14, face = "bold", color = "black")  # Bold axis labels
+  ) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01)) + 
+  scale_x_continuous(labels = scales::number_format(accuracy = 0.01))
 
 
+###Box Plot for EPS per country
+ggplot(EPS_data_sub, aes(x = REF_AREA, y = OBS_VALUE)) +
+  geom_boxplot(fill = "steelblue", color = "black", outlier.colour = "red", outlier.size = 2) +
+  labs(title = "Distribution of EPS by Country (2010-2020)", 
+       y = "EPS", 
+       x = "Country") +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.y = element_text(color = "black", size = 12),  # Y-axis text formatting
+    axis.text.x = element_text(color = "black", size = 12, angle = 45, hjust = 1),  # X-axis formatting
+    axis.title = element_text(size = 14, face = "bold", color = "black")  # Bold axis labels
+  ) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01))  # Format y-axis to 2 decimals
 
+###Average EPS per country
+EPS_sub_avg <- EPS_data_sub %>%
+  group_by(REF_AREA) %>%
+  summarize(avg_EPS = mean(OBS_VALUE, na.rm = TRUE)) #use summarize to collapse data
+View(EPS_sub_avg)
 
+ggplot(EPS_sub_avg, aes(x = REF_AREA, y = avg_EPS)) +
+  geom_bar(stat = "identity", fill = "steelblue", color = "black") +
+  labs(title = "Average EPS by Country (2010-2020)",
+       x = "Country",
+       y = "Mean EPS") +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.y = element_text(color = "black", size = 12),  # Y-axis text formatting
+    axis.text.x = element_text(color = "black", size = 12, angle = 45, hjust = 1),  # X-axis formatting
+    axis.title = element_text(size = 14, face = "bold", color = "black")  # Bold axis labels
+  ) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.01))  # Format y-axis to 2 decimals
 
 
