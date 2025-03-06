0742353d75baa8ce8c0725ae07b1a14a0790cc6a
Hannes Wilkovits <hannes.wilko@gmx.at>
2025-03-06 23:49:24 +0100

    desc stats EPIC - next steps

--- code/desc_stats_EPIC.R
+++ code/desc_stats_EPIC.R
@@ -6,9 +6,21 @@
 #packages
 pacman::p_load("dplyr","ggplot2","tidyverse","haven","data.table","tidyr","jsonlite")
 
-## OECD carbon rate
+##OECD carbon rate
 epic_data <- read.csv("./processed_data/epic_data_var_interest.csv")
 glimpse(epic_data)
+###Variables of interest for core message of research project
+###Subsample for HHs where adoption of EETs is feasible --> see Hassett et al. (2024) pp.43
+###EET adoption
+###Why not EET
+###Drivers for adopting EET
+###income
+###government support
+###home ownership
+###detached housing
+###environmental attitude
+###country - ref area
+###weights
 
 
 
