1d125171188a02e9dfe13a772d08636ce49b0f29
Hannes Wilkovits <hannes.wilko@gmx.at>
2025-05-17 19:24:48 +0200

    country-specific EPS effects

--- code/BHM_Appliances_draft.R
+++ code/BHM_Appliances_draft.R
@@ -68,7 +68,7 @@ pnorm(0, mean = m, sd = s)
 ## 3.3 model2 weighted, random effects -----------------------------------------
 options(mc.cores = 4) #for speeding up computation when working with models or imputation tasks that support parallelization
 
-fitAppliances_m2 <- stan_glmer(
+fitAppliancesw <- stan_glmer(
   Adoption ~ Age_cat + Female + Income + Higher_edu + Home_ownership + 
     Dwelling_house + Dwelling_size + Rural + Env_concern + Gov_support + 
     (1 | Country_name),
@@ -80,21 +80,21 @@ fitAppliances_m2 <- stan_glmer(
 )
 
 ## Diagnostic Plots 
-bayesplot::mcmc_trace(fitAppliances_m2)
+bayesplot::mcmc_trace(fitAppliancesw)
 bayesplot::mcmc_acf_bar(
-  as.array(fitAppliances_m2), 
+  as.array(fitAppliancesw), 
   pars = c("Incomequintile 2", "Incomequintile 3", "Incomequintile 4", "Incomequintile 5"),
   lags = 10
 ) #check per variable or group of variables to increase visibility
-bayesplot::mcmc_hist(fitAppliances_m2)
+bayesplot::mcmc_hist(fitAppliancesw)
 
 ## Summary Results
-summary(fitAppliances_m2)
-posterior_interval(fitAppliances_m2,prob=0.95)
+summary(fitAppliancesw)
+posterior_interval(fitAppliancesw,prob=0.95)
 
 ## Posterior predictive plot and Bayesian p-value 
 Adoption <- appliances$Adoption
-Adoption_rep <- posterior_predict(fitAppliances_m2,draws=1000)
+Adoption_rep <- posterior_predict(fitAppliancesw,draws=1000)
 ppc_stat(Adoption, Adoption_rep, stat = "mean")
 pval <- mean(apply(Adoption_rep, 1, mean) > mean(Adoption))
 pval
@@ -102,7 +102,7 @@ pval
 ## Probability estimate is non-zero
 
 #income
-mat <- as.matrix(fitAppliances_m2$stan_summary)
+mat <- as.matrix(fitAppliancesw$stan_summary)
 m <- mat["Incomequintile 2","mean"]
 s <- mat["Incomequintile 2", "sd"]
 pnorm(0, mean = m, sd = s)
@@ -112,7 +112,7 @@ pnorm(0, mean = m, sd = s)
 pnorm(0, mean = m, sd = s, lower.tail = FALSE)
 
 #government support
-mat <- as.matrix(fitAppliances_m2$stan_summary)
+mat <- as.matrix(fitAppliancesw$stan_summary)
 m <- mat["Gov_support","mean"]
 s <- mat["Gov_support", "sd"]
 #prob <0
@@ -120,8 +120,7 @@ pnorm(0, mean = m, sd = s)
 #prob >0
 pnorm(0, mean = m, sd = s, lower.tail = FALSE)
 
-
-## 3.4 model3 weighted, level-2 predictor -----------------------------------------
+## 3.4 model3 weighted, level-2 predictor: varying intercept -------------------
 options(mc.cores = 4) #for speeding up computation when working with models or imputation tasks that support parallelization
 
 fitAppliances_m3 <- stan_glmer(
@@ -138,21 +137,21 @@ fitAppliances_m3 <- stan_glmer(
 prior_summary(fitAppliances_m3)
 
 ## Diagnostic Plots 
-bayesplot::mcmc_trace(fitAppliances_m2)
+bayesplot::mcmc_trace(fitAppliances_m3)
 bayesplot::mcmc_acf_bar(
-  as.array(fitAppliances_m2), 
+  as.array(fitAppliances_m3), 
   pars = c("Incomequintile 2", "Incomequintile 3", "Incomequintile 4", "Incomequintile 5"),
   lags = 10
 ) #check per variable or group of variables to increase visibility
-bayesplot::mcmc_hist(fitAppliances_m2)
+bayesplot::mcmc_hist(fitAppliances_m3)
 
 ## Summary Results
-summary(fitAppliances_m2)
-posterior_interval(fitAppliances_m2,prob=0.95)
+summary(fitAppliances_m3)
+posterior_interval(fitAppliances_m3,prob=0.95)
 
 ## Posterior predictive plot and Bayesian p-value 
 Adoption <- appliances$Adoption
-Adoption_rep <- posterior_predict(fitAppliances_m2,draws=1000)
+Adoption_rep <- posterior_predict(fitAppliances_m3,draws=1000)
 ppc_stat(Adoption, Adoption_rep, stat = "mean")
 pval <- mean(apply(Adoption_rep, 1, mean) > mean(Adoption))
 pval
@@ -160,19 +159,92 @@ pval
 ## Probability estimate is non-zero
 
 #income
-mat <- as.matrix(fitAppliances_m2$stan_summary)
+mat <- as.matrix(fitAppliances_m3$stan_summary)
 m <- mat["Incomequintile 2","mean"]
 s <- mat["Incomequintile 2", "sd"]
-pnorm(0, mean = m, sd = s)
 #prob <0
 pnorm(0, mean = m, sd = s)
 #prob >0
 pnorm(0, mean = m, sd = s, lower.tail = FALSE)
 
 #government support
-mat <- as.matrix(fitAppliances_m2$stan_summary)
+mat <- as.matrix(fitAppliances_m3$stan_summary)
 m <- mat["Gov_support","mean"]
 s <- mat["Gov_support", "sd"]
+#prob <0
+pnorm(0, mean = m, sd = s)
+#prob >0
+pnorm(0, mean = m, sd = s, lower.tail = FALSE)
+
+#EPS
+mat <- as.matrix(fitAppliances_m3$stan_summary)
+m <- mat["EPS","mean"]
+s <- mat["EPS", "sd"]
+#prob <0
+pnorm(0, mean = m, sd = s)
+#prob >0
+pnorm(0, mean = m, sd = s, lower.tail = FALSE)
+
+## 3.5 model4 weighted, level-2 predictor: varying intercept and slopes --------
+options(mc.cores = 4) #for speeding up computation when working with models or imputation tasks that support parallelization
+
+fitAppliances_m4 <- stan_glmer(
+  Adoption ~ Age_cat + Female + Income + Higher_edu + Home_ownership + 
+    Dwelling_house + Dwelling_size + Rural + Env_concern + Gov_support +
+    EPS + (1 + EPS | Country_name),
+  family = binomial(link = "logit"),
+  prior_covariance = decov(regularization = 3),
+  iter = 2000, warm = 1000, thin = 1,
+  weights = weight_2,
+  data = appliances
+)
+
+prior_summary(fitAppliances_m4)
+
+## Diagnostic Plots 
+bayesplot::mcmc_trace(fitAppliances_m4)
+bayesplot::mcmc_acf_bar(
+  as.array(fitAppliances_m4), 
+  pars = c("Incomequintile 2", "Incomequintile 3", "Incomequintile 4", "Incomequintile 5"),
+  lags = 10
+) #check per variable or group of variables to increase visibility
+bayesplot::mcmc_hist(fitAppliances_m4)
+
+## Summary Results
+summary(fitAppliances_m4)
+posterior_interval(fitAppliances_m4,prob=0.95)
+
+## Posterior predictive plot and Bayesian p-value 
+Adoption <- appliances$Adoption
+Adoption_rep <- posterior_predict(fitAppliances_m4,draws=1000)
+ppc_stat(Adoption, Adoption_rep, stat = "mean")
+pval <- mean(apply(Adoption_rep, 1, mean) > mean(Adoption))
+pval
+
+## Probability estimate is non-zero
+
+#income
+mat <- as.matrix(fitAppliances_m4$stan_summary)
+m <- mat["Incomequintile 2","mean"]
+s <- mat["Incomequintile 2", "sd"]
+#prob <0
+pnorm(0, mean = m, sd = s)
+#prob >0
+pnorm(0, mean = m, sd = s, lower.tail = FALSE)
+
+#government support
+mat <- as.matrix(fitAppliances_m4$stan_summary)
+m <- mat["Gov_support","mean"]
+s <- mat["Gov_support", "sd"]
+#prob <0
+pnorm(0, mean = m, sd = s)
+#prob >0
+pnorm(0, mean = m, sd = s, lower.tail = FALSE)
+
+#EPS
+mat <- as.matrix(fitAppliances_m4$stan_summary)
+m <- mat["EPS","mean"]
+s <- mat["EPS", "sd"]
 #prob <0
 pnorm(0, mean = m, sd = s)
 #prob >0
